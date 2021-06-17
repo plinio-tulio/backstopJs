6f9ad8f6bd63a03f1085878ab8c704cbf8999475
plinio-tulio <pliniotuliosi@gmail.com>
2021-06-16 21:31:42 -0300

    Melhorando o código para reutilizar comandos já implementados.

--- backstop_data/engine_scripts/puppet/custom/successfullySubmitForm.js
+++ backstop_data/engine_scripts/puppet/custom/successfullySubmitForm.js
@@ -1,7 +1,6 @@
+const fillMandatoryFields = require("./fillAllMandatoryFields");
+
 module.exports = async page => {
-    await page.type("#first-name","Plinio");
-    await page.type("#last-name","Eduardo");
-    await page.type("#email","plinio-eduardo@example.com");
-    await page.click("#agree");
+    await fillMandatoryFields(page);
     await page.click("button[type=\"submit\"]");
 }
